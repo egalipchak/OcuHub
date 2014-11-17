3722e4bc6ae7ce42cd7214cf7b60a455c797ba8a
Aaron Wong <kicked1102@gmail.com>
2014-11-16 16:06:06 -0800

    Changed up/down buttons in vertical version. Changed arrows to white.

--- js/vertical.js
+++ js/vertical.js
@@ -1,13 +1,26 @@
 var currentPage = 0;
+var perPage = 8;
+
 var menu = JSON.parse(localStorage["menu"]);
 var index = JSON.parse(localStorage["index"]);
-var maxPages = menu.length/8;
+var maxPages = menu.length/perPage;
 
 $('#container .rectangle').hover(        	
     function(e) {
         var element = $(this);
         var progressbar = $(element).find('.progress');
+
 		progressbar.show();
+
+        this.onDone = setTimeout(function(){
+            element.click();
+            
+            progressbar.hide();
+            clearInterval(this.interval);
+            clearInterval(this.onDone);
+            progressbar.css('webkitAnimationName', '');
+        },2000);
+
         this.interval = setTimeout(function(){
             progressbar.css('webkitAnimationName', 'progress');
         },500);
@@ -16,15 +29,16 @@ $('#container .rectangle').hover(
         var progressbar = $(this).find('.progress');
         progressbar.hide();
         clearInterval(this.interval);
+        clearInterval(this.onDone);
         progressbar.css('webkitAnimationName', '');
     }
 );
 
 function loadMenu(pageNo) {
     var i;
-    var offset = pageNo*8;
-    for(i=0+offset; i<8+offset; i++){
-        var menuItem = $('#container .rectangle')[i-offset];
+    var offset = pageNo*perPage;
+    for(i=0+offset; i<perPage+offset; i++){
+        var menuItem = $('#container .rectangle')[i-offset + 1];
         $(menuItem).attr('id', menu[i].id);
         var menuImage = "images/menu/" + menu[i].id;
         $(menuItem).css('background-image', 'url('+menuImage +'.png)');
@@ -42,35 +56,34 @@ function setPageNumbers(){
     }
     
 }
-$('#right').hover(
+$('#prevPage').click(
     function(){
         this.interval = setTimeout(function(){
-            if(currentPage < maxPages-1){
-                currentPage++;
-                $('#container').hide();
-                $('#container').slideDown("slow");
-                loadMenu(currentPage);
-            }
-        },1000);
-    },
-    function(){
-        clearInterval(this.interval);
+
+            ++currentPage;
+            currentPage %= maxPages;
+
+            $('#container').hide();
+            $('#container').slideDown("slow");
+            loadMenu(currentPage);
+            
+        },100);
     }
 );
 
-$('#left').hover(
+$('#nextPage').click(
     function(){
         this.interval = setTimeout(function(){
-            if(currentPage > 0){
-                currentPage--;            
-                $('#container').hide();
-                $('#container').slideDown("slow");
-                loadMenu(currentPage);
-            }
-        },1000);
-    },
-    function(){
-        clearInterval(this.interval);
+            
+            --currentPage;
+            if (currentPage < 0)
+                currentPage = maxPages-1;
+
+            $('#container').hide();
+            $('#container').slideDown("slow");
+            loadMenu(currentPage);
+            
+        },100);
     }
 );
 
